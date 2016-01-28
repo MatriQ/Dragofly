0b31f40454a22013cbf281422c0f804576f7ad26
matri@BDPC <matrixdom@126.com>
2016-01-28 12:19:15 +0800

    add User api

---
+++ api/controllers/UserController.js
@@ -0,0 +1,21 @@
+/**
+ * UserController
+ *
+ * @description :: Server-side logic for managing Users
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	get:function(){
+
+	},
+	add:function(){
+
+	},
+	modify:function(){
+
+	},
+	delete:function{
+
+	}
+};
