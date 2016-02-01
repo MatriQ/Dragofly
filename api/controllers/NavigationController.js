ae819a2c30730e251753477304713e6cc22f0665
matri@BDPC <matrixdom@126.com>
2016-02-01 14:56:43 +0800

    add navigation controller

---
+++ api/controllers/NavigationController.js
@@ -0,0 +1,57 @@
+/**
+ * NavigationController
+ *
+ * @description :: Server-side logic for managing navigations
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+		index:function(req,res){
+			Navigation.find().then(function(navs){
+				res.locals.navs=navs;
+				return res.view();
+			}).catch(function(err){
+				req.flash('message',err);
+				return res.view();
+			});
+		},
+		new:function(req,res){
+			var newNav={
+				name:'',
+		    icon:'',
+		    tag:'',
+		    desc:'',
+		    sort:0
+			};
+			if (req.method=='GET') {
+				res.locals.nav=newNav;
+				return res.view();
+			}
+			else if (req.method=='POST') {
+				newNav={
+					name:req.param('name'),
+			    icon:req.param('icon'),
+			    tag:req.param('tag'),
+			    desc:req.param('desc'),
+			    sort:parseInt(req.param('sort'))
+				};
+				Navigation.create(newNav).then(function(ret){
+					return res.redirect('back');
+				}).catch(function(err){
+					console.log(err);
+					req.flash('message',err);
+					return res.view();
+				});
+			}
+		},
+		edit:function(req,res){
+			var id=req.param('id');
+			Navigation.findOne({id:id}).then(function(nav){
+				res.locals.nav=nav;
+				return res.view();
+			}).catch(function(err){
+				req.flash('message',err);
+				return res.view();
+			});
+		}
+};
--- config/connections.js
+++ config/connections.js
@@ -61,7 +61,7 @@ module.exports.connections = {
     host: 'localhost',
     port: 27017,
     user: 'admin',
-    password: '1231',
+    password: '123',
     database: 'AdminDB'
   },
 
